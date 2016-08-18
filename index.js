0dab9a6edf795f4c96289c960fda41859904999e
John Otander <johnotander@gmail.com>
2016-08-18 16:56:10 -0600

    Initial implementation

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-'use strict'
+const { send } = require('micro')
+const getQueryParam = require('get-query-param')
+const whoThat = require('who-that')
+const isBlank = require('is-blank')
 
-module.exports = function whoThatApi (options) {
-  options = options || {}
+module.exports = async function (req, res) {
+  const fullUrl = `http://foo.bar${req.url}`
 
-  return true
+  const npm = getQueryParam('npm', fullUrl)
+  const github = getQueryParam('github', fullUrl)
+
+  if (isBlank(npm) || isBlank(github)) {
+    return send(res, 406)
+  }
+
+  const who = await whoThat({ npm, github })
+  send(res, 200, who)
 }
